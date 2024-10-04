303c1d010d0667d05436d3e865936b762a650b3f
liuyuweitarek <liuyuwei.tarek@gmail.com>
2024-10-05 02:43:27 +0800

    cicd: del deprecated flows

--- local-build-scripts/build_1.7.1-py3.6-cuda11.0-ubuntu20.04.sh
+++
@@ -1,1 +0,0 @@
-docker build -t liuyuweitarek/pytorch:1.7.1-py3.6-cuda11.0-ubuntu20.04 -f image-builder/docker/Ubuntu-Pip --build-arg PYTHON_VERSION=3.6 --build-arg UBUNTU_VERSION=20.04 --build-arg TORCH_PACKAGE_NAME=1.7.1+cu110 --build-arg TORCH_SOURCE_URL=https://download.pytorch.org/whl/cu110 --no-cache .
